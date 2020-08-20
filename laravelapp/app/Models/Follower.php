9019042841c4e01b247220deea7f36dab2b6d474
teionhozonn <teionhozonn@gmail.com>
2020-08-21 06:11:29 +0900

    01

--- laravelapp/app/Models/Follower.php
+++ laravelapp/app/Models/Follower.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Follower extends Model
 {
-    //
+    protected $primarykey = [
+      'following_id',
+      'followed_id',
+    ];
+
+    protected $fillable = [
+      'following_id',
+      'followed_id',
+    ];
+
+
+    public $timestamps = false;
+    public $incrementing = false;
 }
