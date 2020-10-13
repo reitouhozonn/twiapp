33c3284db31ffba99ad6db6cd9fa6230341da1a1
teionhozonn <teionhozonn@gmail.com>
2020-10-13 15:12:31 +0900

    kansei

--- laravelapp/resources/views/tweets/show.blade.php
+++ laravelapp/resources/views/tweets/show.blade.php
@@ -40,90 +40,77 @@
                         <p class="mb-0 text-secondary">{{ count($tweet->comments) }}</p>
                     </div>
                     <div class="d-flex align-items-center">
-                      @if (!in_array(Auth::user()->id, array_column($timeline->favorites->toArray(), 'user_id'), TRUE))
-                          <form method="POST" action="{{ url('favorites/') }}" class="mb-0">
-                              @csrf
-
-                              <input type="hidden" name="tweet_id" value="{{ $timeline->id }}">
-                              <button type="submit" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
-                          </form>
-                      @else
-                          <form method="POST"action="{{ url('favorites/' .array_column($timeline->favorites->toArray(), 'id', 'user_id')[Auth::user()->id]) }}" class="mb-0">
-                              @csrf
-                              @method('DELETE')
-
-                              <button type="submit" class="btn p-0 border-0 text-danger"><i class="fas fa-heart fa-fw"></i></button>
-                          </form>
-                      @endif
-                      <p class="mb-0 text-secondary">{{ count($timeline->favorites) }}</p>
+                        <button type="" class="btn p-0 border-0 text-primary"><i class="far fa-heart fa-fw"></i></button>
+                        <p class="mb-0 text-secondary">{{ count($tweet->favorites) }}</p>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 
+    <!-- ここから下を変更してください -->
     <div class="row justify-content-center">
-            <div class="col-md-8 mb-3">
-                <ul class="list-group">
-                    @forelse ($comments as $comment)
-                        <li class="list-group-item">
-                            <div class="py-3 w-100 d-flex">
-                                <img src="{{ asset('storage/profile_image/' .$comment->user->profile_image) }}" class="rounded-circle" width="50" height="50">
-                                <div class="ml-2 d-flex flex-column">
-                                    <p class="mb-0">{{ $comment->user->name }}</p>
-                                    <a href="{{ url('users/' .$comment->user->id) }}" class="text-secondary">{{ $comment->user->screen_name }}</a>
-                                </div>
-                                <div class="d-flex justify-content-end flex-grow-1">
-                                    <p class="mb-0 text-secondary">{{ $comment->created_at->format('Y-m-d H:i') }}</p>
-                                </div>
-                            </div>
-                            <div class="py-3">
-                                {!! nl2br(e($comment->text)) !!}
-                            </div>
-                        </li>
-                    @empty
-                        <li class="list-group-item">
-                            <p class="mb-0 text-secondary">コメントはまだありません。</p>
-                        </li>
-                    @endforelse
+        <div class="col-md-8 mb-3">
+            <ul class="list-group">
+                @forelse ($comments as $comment)
                     <li class="list-group-item">
+                        <div class="py-3 w-100 d-flex">
+                            <img src="{{ asset('storage/profile_image/' .$comment->user->profile_image) }}" class="rounded-circle" width="50" height="50">
+                            <div class="ml-2 d-flex flex-column">
+                                <p class="mb-0">{{ $comment->user->name }}</p>
+                                <a href="{{ url('users/' .$comment->user->id) }}" class="text-secondary">{{ $comment->user->screen_name }}</a>
+                            </div>
+                            <div class="d-flex justify-content-end flex-grow-1">
+                                <p class="mb-0 text-secondary">{{ $comment->created_at->format('Y-m-d H:i') }}</p>
+                            </div>
+                        </div>
                         <div class="py-3">
-                            <form method="POST" action="{{ route('comments.store') }}">
-                                @csrf
-
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-12 p-3 w-100 d-flex">
-                                        <img src="{{ asset('storage/profile_image/' .$user->profile_image) }}" class="rounded-circle" width="50" height="50">
-                                        <div class="ml-2 d-flex flex-column">
-                                            <p class="mb-0">{{ $user->name }}</p>
-                                            <a href="{{ url('users/' .$user->id) }}" class="text-secondary">{{ $user->screen_name }}</a>
-                                        </div>
-                                    </div>
-                                    <div class="col-md-12">
-                                        <input type="hidden" name="tweet_id" value="{{ $tweet->id }}">
-                                        <textarea class="form-control @error('text') is-invalid @enderror" name="text" required autocomplete="text" rows="4">{{ old('text') }}</textarea>
-
-                                        @error('text')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                    </div>
-                                </div>
-
-                                <div class="form-group row mb-0">
-                                    <div class="col-md-12 text-right">
-                                        <p class="mb-4 text-danger">140文字以内</p>
-                                        <button type="submit" class="btn btn-primary">
-                                            ツイートする
-                                        </button>
-                                    </div>
-                                </div>
-                            </form>
+                            {!! nl2br(e($comment->text)) !!}
                         </div>
                     </li>
-                </ul>
-            </div>
+                @empty
+                    <li class="list-group-item">
+                        <p class="mb-0 text-secondary">コメントはまだありません。</p>
+                    </li>
+                @endforelse
+                <li class="list-group-item">
+                    <div class="py-3">
+                        <form method="POST" action="{{ route('comments.store') }}">
+                            @csrf
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-12 p-3 w-100 d-flex">
+                                    <img src="{{ asset('storage/profile_image/' .$user->profile_image) }}" class="rounded-circle" width="50" height="50">
+                                    <div class="ml-2 d-flex flex-column">
+                                        <p class="mb-0">{{ $user->name }}</p>
+                                        <a href="{{ url('users/' .$user->id) }}" class="text-secondary">{{ $user->screen_name }}</a>
+                                    </div>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="hidden" name="tweet_id" value="{{ $tweet->id }}">
+                                    <textarea class="form-control @error('text') is-invalid @enderror" name="text" required autocomplete="text" rows="4">{{ old('text') }}</textarea>
+
+                                    @error('text')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-12 text-right">
+                                    <p class="mb-4 text-danger">140文字以内</p>
+                                    <button type="submit" class="btn btn-primary">
+                                        ツイートする
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </li>
+            </ul>
         </div>
     </div>
-    @endsection
+</div>
+@endsection
